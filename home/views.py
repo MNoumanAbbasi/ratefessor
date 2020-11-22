cd45db4df9e39bf2c97698e93a3176fc494972c6
Cypharris <harisnoor674@gmail.com>
2020-11-22 11:03:27 +0500

    Added Sign in, Sign up and home page html

--- home/views.py
+++ home/views.py
@@ -10,8 +10,17 @@ profiles = [
 ]
 
 
+# def home(request):
+#     context = {
+#         'profiles': profiles
+#     }
+#     return render(request, 'home/home.html', context)
+
 def home(request):
-    context = {
-        'profiles': profiles
-    }
-    return render(request, 'home/home.html', context)
+    return render(request, 'home/index.html')
+
+def signin(request):
+    return render(request, 'home/signin.html')
+
+def signup(request):
+    return render(request, 'home/signup.html')
