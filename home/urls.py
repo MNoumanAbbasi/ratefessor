d7cd6bc57e0ebd84d168470c6abf928d85ff18cf
Hira Jamshed <hirajamshed000@gmail.com>
2020-11-26 20:51:34 +0300

    Added buttons

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # path('admin', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', views.home, name='app-home'),
     path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
