84ae44fbaed65d5df4b1b865d141d05c4d1487cc
Nouman Abbasi <thenoumanabbasi@gmail.com>
2020-11-27 03:23:08 +0500

    add: courses and review section on proff profile

--- profiles/views.py
+++ profiles/views.py
@@ -13,15 +13,49 @@ def professor(request, id):
     # If no professor found with that id
     if row is None:
         return HttpResponseBadRequest('No professor found!')
-    else:
-        context = {
-            'id': row[0],
-            'name': row[1],
-            'position': row[2],
-            'dept_name': row[3],
-            'qualification': row[4]
-        }
-        return render(request, 'profiles/professor.html', context)
+
+    professor = {
+        'id': row[0],
+        'name': row[1],
+        'position': row[2],
+        'dept_name': row[3],
+        'qualification': row[4]
+    }
+
+    # Now fetching all the courses this professor offered
+    cursor.execute("SELECT * FROM prof_sec WHERE prof_id = ?;", (id,))
+    courses = [{'name': r[1], 'semester': r[2], 'year': r[3]}
+               for r in cursor.fetchall()]
+
+    # TODO: Remove afterwards. Hard coded for testing:
+    courses.append({'name': 'Intro. to Programming',
+                    'semester': 'Fall', 'year': 2019})
+    courses.append({'name': 'Intro. to Programming',
+                    'semester': 'Fall', 'year': 2019})
+    courses.append({'name': 'Databases',
+                    'semester': 'Fall', 'year': 2019})
+    courses.append({'name': 'Intro. to Computer Science',
+                    'semester': 'Fall', 'year': 2019})
+    courses.append({'name': 'Probability',
+                    'semester': 'Fall', 'year': 2019})
+
+    # Fetching reviews for this professor
+    cursor.execute("SELECT * FROM review WHERE prof_id = ?;", (id,))
+    reviews = [{'user_id': r[1], 'text': r[2], 'date': r[3], 'course_name': r[5],
+                'semester': r[6], 'year': r[7]} for r in cursor.fetchall()]
+
+    # TODO: Remove afterwards. Hard coded for testing:
+    reviews.append({'user_id': 1, 'text': 'Very good instructor. Recommended',
+                    'date': '2020-10-2', 'course_name': 'Intro. to Programming',
+                    'semester': 'Fall', 'year': 2019})
+
+    context = {
+        'professor': professor,
+        'courses': courses,
+        'reviews': reviews
+    }
+
+    return render(request, 'profiles/professor.html', context)
 
 
 def course(request, course_name):
