02fc0d524b4c70fe4440f4943376161fc4cd3fe4
Hira Jamshed <hirajamshed000@gmail.com>
2020-11-28 22:46:18 +0300

    Made changes after merge

--- search/urls.py
+++ search/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search_professor', views.search_professor, name='search_professor'),
-    path('search_course', views.search_course, name='search_course'),
-    path('search_professor_course', views.search_combo, name='search_combo'),
+    path('search_professor', views.search_professor, name='search-professor'),
+    path('search_course', views.search_course, name='search-course'),
+    path('search_professor_course', views.search_combo, name='search-combo'),
 ]
