c66d0f77c99d01ca836abc64954dd5c897f90728
mudbri <mubashiranwar@live.com>
2020-11-28 23:34:13 +0500

    done

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from . import views
+
+# app_name = 'home'
 
 urlpatterns = [
     # path('admin', admin.site.urls),
     path('', views.home, name='app-home'),
-    path('signin', views.signin, name='signin'),
-    path('signup', views.signup, name='signup'),
+    re_path(r'^review/$', views.home_review, name="review"),
+    # re_path(r'^signin/$', views.signin_view, name="signin"),
+    # re_path(r'^signout/$', views.signout_view, name="signout"),
+
+    # path('signin', views.signin, name='signin'),
+    # path('signup', views.signup, name='signup'),
 
 ]
