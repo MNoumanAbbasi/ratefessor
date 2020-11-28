2e80ed96c94b178b89897c68cf1afff73f2f9224
Nouman Abbasi <thenoumanabbasi@gmail.com>
2020-11-28 21:15:34 +0500

    add: major file structure change + base html setup

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='app-home'),
     path('professor/<id>', views.professor, name='professor-profile'),
     path('course/<course_name>', views.course, name='course-profile'),
 ]
